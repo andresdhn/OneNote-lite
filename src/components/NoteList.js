b725175fb4deb207f17af29c09f792b83ed81961
Andres <hernandez.andres.d@gmail.com>
2020-01-28 15:46:47 +1100

    Adds animation and styles to notelist

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 //
 export default props => {
     const handleClick = e => {
@@ -6,17 +7,19 @@ export default props => {
     }
 
     return (
-        <div id="notelist">
-            {props.list.map(note => (
-                <article
-                    key={note.id}
-                    id={note.id}
-                    className="note"
-                    onClick={handleClick}
-                >
-                    {note.title}
-                </article>
-            ))}
-        </div>
+        <Fade top distance={'.8em'} duration={500} cascade>
+            <div id="notelist">
+                {props.list.map(note => (
+                    <article
+                        key={note.id}
+                        id={note.id}
+                        className="note"
+                        onClick={handleClick}
+                    >
+                        {note.title}
+                    </article>
+                ))}
+            </div>
+        </Fade>
     )
 }
