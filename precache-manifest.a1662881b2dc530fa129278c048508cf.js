c9474743026c15a7ac73450b6705018656599f6d
Andres <hernandez.andres.d@gmail.com>
2020-02-14 14:53:05 +1100

    Updates

---
+++ precache-manifest.a1662881b2dc530fa129278c048508cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4757e30b0bb932ac81166b1fcff9f2c9",
+    "url": "/OneNote-lite/index.html"
+  },
+  {
+    "revision": "577bdb89ad57c9a412e7",
+    "url": "/OneNote-lite/static/css/main.3d7ec150.chunk.css"
+  },
+  {
+    "revision": "6d750598a921dcb2018b",
+    "url": "/OneNote-lite/static/js/2.ffa0851e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/OneNote-lite/static/js/2.ffa0851e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "577bdb89ad57c9a412e7",
+    "url": "/OneNote-lite/static/js/main.4fe72f67.chunk.js"
+  },
+  {
+    "revision": "e818bb6f8e0a97a911d3",
+    "url": "/OneNote-lite/static/js/runtime-main.c8cb9f8b.js"
+  }
+]);
